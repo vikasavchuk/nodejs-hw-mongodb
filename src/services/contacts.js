026e4886885a5879d6fc817ef72568481f91ad7c
vikasavchuk <s.vika02030@gmail.com>
2024-11-12 17:57:53 +0200

    add file

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,25 @@
 import { ContactsCollection } from '../models/contact.js';
 
-export const getAllContacts = async () => await ContactsCollection.find();
+export const getAllContacts = async (page = 1, perPage = 10, sort = {}, filter = {}) => {
+  const skip = (page - 1) * perPage;
+  const totalItems = await ContactsCollection.countDocuments(filter);
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  const contacts = await ContactsCollection.find(filter)
+    .sort(sort)
+    .skip(skip)
+    .limit(perPage);
+
+  return {
+    data: contacts,
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    hasPreviousPage: page > 1,
+    hasNextPage: page < totalPages,
+  };
+};
 
 export const getContactById = async (contactId) => await ContactsCollection.findById(contactId);
 
